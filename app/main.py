16d22c6dff46613f4ce763cb5f770b8fb3afb1b3
franciscoantoniosanchezdiaz15 <franciscoantoniosanchezdiaz15@gmail.com>
2025-10-03 17:25:18 +0200

    feat: cargar json al arrancar servidor

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, current_app
+import json
 
 app = Flask(__name__)
 
+with open("data/pokemon.json", encoding="utf-8") as f:
+    app.config["DATA"] = json.load(f)
 
 @app.route("/")
 def bienvenida():
     return render_template("bienvenida.html")
 
+@app.route("/data")
+def home():
+    return jsonify(current_app.config["DATA"])
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 8080, debug=True)
