be25f61c14b5dcd8f93be14d97abadfc73c1fa6d
Daniel Martínez Fernández <daniel.martinezferna@WIRTZ.LOCAL>
2025-10-06 17:37:52 +0200

    refactor: cambiar nombre bienvenida a index

--- app/main.py
+++ app/main.py
@@ -6,14 +6,16 @@ app = Flask(__name__)
 with open("data/pokemon.json", encoding="utf-8") as f:
     app.config["DATA"] = json.load(f)
 
+
 @app.route("/")
 def bienvenida():
-    return render_template("bienvenida.html")
+    return render_template("index.html")
+
 
 @app.route("/data")
 def home():
     return jsonify(current_app.config["DATA"])
 
+
 if __name__ == "__main__":
     app.run("0.0.0.0", 8080, debug=True)
-    
