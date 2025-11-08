751401ed2093300ac7ed716534b58356c8d085aa
Daniel Martínez Fernández <mfdanielmf@gmail.com>
2025-11-08 21:05:29 +0100

    chore: crear servicio movimientos aleatorios

--- app/services/battle_service.py
+++ app/services/battle_service.py
@@ -1,3 +1,13 @@
-#hacer le buscaodor de random pokemon y enemigo igual q su imgen
+import random
+from app.services.pokemon_service import pokemon_existe
 
-#random mocves
+# hacer le buscaodor de random pokemon y enemigo igual q su imgen
+
+
+def random_moves(nombre):
+    pokemon = pokemon_existe(nombre)
+
+    movimientos = random.sample(
+        pokemon.moves, min(4, len(pokemon.moves)))
+
+    return movimientos
