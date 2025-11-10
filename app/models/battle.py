0fd5af139c38a517b9618b987e685f8b3bbd6945
franciscoantoniosanchezdiaz15 <franciscoantoniosanchezdiaz15@gmail.com>
2025-11-10 17:48:17 +0100

    feat:class battle

---
+++ app/models/battle.py
@@ -0,0 +1,10 @@
+class Battle:
+    def __init__(self, turno, datos_pokemon_jugador, datos_pokemon_rival, log, vida_jugador, vida_rival, ataques_jugador, ataques_rival):
+        self.turno = turno
+        self.datos_pokemon_jugador = datos_pokemon_jugador
+        self.datos_pokemon_rival = datos_pokemon_rival
+        self.log = log
+        self.vida_jugador = vida_jugador
+        self.vida_rival = vida_rival
+        self.ataques_jugador = ataques_jugador
+        self.ataques_rival = ataques_rival
