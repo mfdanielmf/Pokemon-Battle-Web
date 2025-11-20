59ae72955cd252f422ac21d9e1d31eaa75f6e622
franciscoantoniosanchezdiaz15 <franciscoantoniosanchezdiaz15@gmail.com>
2025-11-20 18:01:14 +0100

    feat: hecha base de datos pokemons

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask
 from flask_session import Session
 
@@ -19,6 +20,20 @@ app.register_blueprint(home_bp, url_prefix="/")
 app.register_blueprint(pokemon_bp, url_prefix="/pokemons")
 app.register_blueprint(battle_bp, url_prefix="/battle")
 
+def get_connection():
+    conn = sqlite3.connect("data/pokemons.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/productos")
+def listar_productos():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT id, nombre, precio FROM productos ORDER BY id;")
+    productos = cur.fetchall()  #obtener los productos en una lista
+    cur.close()
+    conn.close()
+    return render_template("productos.html", productos=productos)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 8080, debug=True)
