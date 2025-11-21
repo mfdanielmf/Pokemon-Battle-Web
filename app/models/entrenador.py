fb682d8fbb0cfa355993c08655b7d16ea3fdb194
franciscoantoniosanchezdiaz15 <franciscoantoniosanchezdiaz15@gmail.com>
2025-11-21 17:51:19 +0100

    feat: archivo db.py

--- app/models/entrenador.py
+++ app/models/entrenador.py
@@ -1,5 +1,8 @@
+from sqlalchemy import Column, Integer, String
 from app.database import db
 
 class Entidad(db.Model):
     __tablename__ = "entrenador"
-     id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), unique=True, nullable=False)
+    contraseña = Column(String(100))
