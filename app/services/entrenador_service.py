69eeeee734d9299a4024be0f35136d4d34c684e1
franciscoantoniosanchezdiaz15 <franciscoantoniosanchezdiaz15@gmail.com>
2025-11-27 16:31:04 +0100

    feat: entrenador_service

--- app/services/entrenador_service.py
+++ app/services/entrenador_service.py
@@ -2,8 +2,11 @@ import app.repositories.entrenador_repo as entrenador_repo
 
 def registrar_entrenador():
     entrenador_repo.crear_entrenador()
-    return
+    return "Se ha creado el entrenador"
 
-def autenticar_entrenador():
-    
-    return entrenador_repo.obtener_entrenador_por_nombre()
+def autenticar_entrenador(nombre, contraseña):
+    entrenador_aut = entrenador_repo.obtener_entrenador_por_nombre(nombre)
+    contraseña = entrenador_repo.Entrenador.check_Password(contraseña)
+    if entrenador_aut and contraseña:
+        return entrenador_aut
+    return None
