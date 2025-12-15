d44a03a5d1820a572165dc4852099a8d68f52748
Daniel Martínez Ferández <mfdanielmf@gmail.com>
2025-12-15 18:22:14 +0100

    feat: modificar ruta historial batallas para aprovechar session

--- app/routes/entrenador_routes.py
+++ app/routes/entrenador_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, render_template
+from flask import Blueprint, redirect, render_template, session, url_for
 
 from app.models.exceptions import EntrenadorNotFoundException, JugadorSinBatallasException
 from app.services.battle_service import obtener_todas_batallas_id_entrenador
@@ -8,11 +8,13 @@ from app.services.current_year_service import get_current_year
 entrenador_bp = Blueprint("entrenador", __name__)
 
 
-@entrenador_bp.route("/historial-batallas/<int:id_entrenador>")
-def historial_batallas(id_entrenador):
+@entrenador_bp.route("/historial-batallas")
+def historial_batallas():
+    id_entrenador = session.get("entrenador_id")
+
     try:
         batallas = obtener_todas_batallas_id_entrenador(id_entrenador)
     except EntrenadorNotFoundException or JugadorSinBatallasException:
-        return redirect("pokemon.lista")
+        return redirect(url_for("home.index"))
 
     return render_template("historial_batallas.html", batallas=batallas, year=get_current_year())
