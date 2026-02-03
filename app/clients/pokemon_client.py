452f907654ee0be14d80a44894ce2fbf8ecf3410
Daniel Martínez Ferández <mfdanielmf@gmail.com>
2026-02-03 17:37:52 +0100

    feat: añadir limite tamaño cache

--- app/clients/pokemon_client.py
+++ app/clients/pokemon_client.py
@@ -1,14 +1,17 @@
+from collections import OrderedDict
 import time
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
 TIEMPO_LIMITE = 300  # 5 min máximo para el cache
+MAX_CACHE = 40
+MAX_CACHE_MOVES = 40
 
 
 class PokemonClient:
     def __init__(self):
-        self._cache = {}
-        self._cacheMoves = {}
+        self._cache = OrderedDict()
+        self._cacheMoves = OrderedDict()
 
     def fetch_pokemon_detail(self, id):  # id o nombre
         if id in self._cache:
@@ -27,12 +30,18 @@ class PokemonClient:
 
             self._cache[id] = data
 
+            # Borramos el primero si pasamos del límite de tamaño
+            if len(self._cache) > MAX_CACHE:
+                self._cache.popitem(last=False)
+
             return data
         except Exception as e:
             print(f"Error desconocido: {str(e)}")
             return None
 
     def fetch_moves_detail(self, url):
+        print(len(self._cacheMoves))
+
         if url in self._cacheMoves:
             data = self._cacheMoves[url]
 
@@ -45,6 +54,10 @@ class PokemonClient:
             data["expiracion"] = time.time()
 
             self._cacheMoves[url] = data
+
+            # Borramos el primer ataque si nos pasamos del tamaño
+            if len(self._cacheMoves) > MAX_CACHE_MOVES:
+                self._cacheMoves.popitem(last=False)
 
             return data
         except Exception as e:
